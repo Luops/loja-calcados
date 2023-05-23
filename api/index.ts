caa0ee53769e219fe253ac82d9c09eddee7853ea
gabeFrancisco <gabegit2@gmail.com>
2023-05-22 21:21:36 -0300

    express, middlewares and dotenv setup

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,25 @@
-console.log("It's working!")
-console.log('Something changed!')
+import express from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    credentials: true
+}))
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
+
+server.listen(8080, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORTA}`)
+})
